0b2685ad2328745fdf4d69bcfd4a142586fbaa98
Alexandre Gauthier <alex@lab.underwares.org>
2017-12-06 05:09:46 -0500

    Update metadata

--- SpaceEngineers/Properties/AssemblyInfo.cs
+++ SpaceEngineers/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("SpaceEngineers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Terrible Garbage for Space Engineers Blocks")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SpaceEngineers")]
